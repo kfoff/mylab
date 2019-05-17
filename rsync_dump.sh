2fab9599a744f6175e195e4c84cf24b204ec058a
discoroveryx <root@babah24.ru>
2019-05-17 14:00:10 +0700

    autocommit_from_discoroveryx

--- rsync_dump.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# shop
-#rsync --progress --delete-before -azv /var/www/shop/na_dump/dump mylab@mylab.babah24.ru::mylab/shop
-#rsync --progress --exclude=CACHE --exclude=syncapp_tmp --delete-before -azv /var/www/shop/media mylab@mylab.babah24.ru::mylab/shop
-
-# crm
-rsync --progress --delete-before -azv /var/www/crmdata/dump mylab@mylab.babah24.ru::mylab/crm
-rsync --progress --exclude=CACHE --exclude=additionalphoto --exclude=buyer_anketa --exclude=cashreportfile --exclude=corpmailfile --exclude=docflowfile --exclude=goodscert_pdf --exclude=librarybook --exclude=materialvalue --delete-before -azv /var/www/crm/media mylab@mylab.babah24.ru::mylab/crm
