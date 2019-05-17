8ad27b12c693eebcdfb301570d5a924292c307fd
discoroveryx <root@babah24.ru>
2019-05-17 13:49:22 +0700

    autocommit_from_discoroveryx

--- rsync_dump.sh
+++ rsync_dump.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # shop
-rsync --progress --exclude=.example --delete-before -azv /var/www/shop/na_dump/dump mylab@mylab.babah24.ru::mylab/shop
-rsync --progress --exclude=CACHE --exclude=syncapp_tmp --delete-before -azv /var/www/shop/media mylab@mylab.babah24.ru::mylab/shop
+#rsync --progress --delete-before -azv /var/www/shop/na_dump/dump mylab@mylab.babah24.ru::mylab/shop
+#rsync --progress --exclude=CACHE --exclude=syncapp_tmp --delete-before -azv /var/www/shop/media mylab@mylab.babah24.ru::mylab/shop
 
 # crm
-rsync --progress --exclude=.example --delete-before -azv /var/www/shop/na_dump/dump mylab@mylab.babah24.ru::mylab/shop
+rsync --progress --delete-before -azv /var/www/crmdata/dump mylab@mylab.babah24.ru::mylab/crm
+#rsync --progress --exclude=additionalphoto --exclude=buyer_anketa --exclude=cashreportfile --exclude=corpmailfile --exclude=docflowfile --exclude=goodscert_pdf --exclude=librarybook --exclude=materialvalue --delete-before -azv /var/www/crm/na_dump/dump mylab@mylab.babah24.ru::mylab/crm
